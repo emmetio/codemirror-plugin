472ec4219c91f771bb1fb78bdcd848f41c126c79
Sergey Chikuyonok <serge.che@gmail.com>
2017-05-17 02:50:35 +0300

    Separate browser bundle builder

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,13 @@
 'use strict';
 
-import nodeResolve from 'rollup-plugin-node-resolve';
-
 export default {
 	entry: './plugin.js',
-	plugins: [ nodeResolve() ],
-	moduleName: 'emmetCodeMirrorPlugin',
+	external: [
+		'@emmetio/expand-abbreviation',
+		'@emmetio/extract-abbreviation'
+	],
 	targets: [
 		{ format: 'cjs', dest: 'dist/emmet-codemirror-plugin.cjs.js' },
 		{ format: 'es',  dest: 'dist/emmet-codemirror-plugin.es.js' },
-		{ format: 'umd',  dest: 'dist/emmet-codemirror-plugin.js' }
 	]
 };

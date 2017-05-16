7cb2ffdabd018f0daacca2171b6d35bbab2638a5
Sergey Chikuyonok <serge.che@gmail.com>
2017-05-16 03:03:24 +0300

    Initial implementation (probably not working)

---
+++ plugin.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import emmetExpandAbbreviation from './lib/commands/expand-abbreviation';
+
+const globalCommands = { emmetExpandAbbreviation };
+
+/**
+ * Adds Emmet support to given CodeMirror editor instance
+ * @param  {CodeMirror} editor
+ * @return {Function} A function that, when called, completely removes Emmet
+ * support from editor instance
+ */
+export default function(editor) {
+	const keymap = {
+		Tab: 'emmetExpandAbbreviation'
+	};
+
+	registerCommands(editor, globalCommands);
+	editor.addKeyMap(keymap);
+
+	return () => {
+		editor.removeKeyMap(keymap);
+	};
+}
+
+/**
+ * Registers editor commands in given editor, if required
+ * @param  {CodeMirror} editor   Editor instance
+ * @param  {Object}     commands Commands map
+ */
+function registerCommands(editor, commands) {
+	const CodeMirror = editor.contructor;
+
+	Object.keys(commands).forEach(name => {
+		if (!CodeMirror.commands[name]) {
+			CodeMirror.commands[name] = commands[name];
+		}
+	});
+}
---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+export default {
+	entry: './plugin.js',
+	plugins: [ nodeResolve() ],
+	moduleName: 'emmetCodeMerrorPlugin',
+	targets: [
+		{ format: 'cjs', dest: 'dist/emmet-codemirror-plugin.cjs.js' },
+		{ format: 'es',  dest: 'dist/emmet-codemirror-plugin.es.js' },
+		{ format: 'umd',  dest: 'dist/emmet-codemirror-plugin.js' }
+	]
+};

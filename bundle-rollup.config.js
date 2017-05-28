bdcb26f24ff46de084c5cdac22c797ce255b6c38
Sergey Chikuyonok <serge.che@gmail.com>
2017-05-29 01:46:31 +0300

    Support custom snippets

--- bundle-rollup.config.js
+++ bundle-rollup.config.js
@@ -2,14 +2,14 @@
 
 import nodeResolve from 'rollup-plugin-node-resolve';
 import buble from 'rollup-plugin-buble';
-// import uglify from 'rollup-plugin-uglify';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
 	entry: './browser.js',
 	plugins: [
 		nodeResolve(),
-		buble()
-		// uglify()
+		buble(),
+		uglify()
 	],
 	moduleName: 'emmetCodeMirrorPlugin',
 	format: 'umd',
--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,8 @@ export default {
 	entry: './extension.js',
 	external: [
 		'@emmetio/expand-abbreviation',
-		'@emmetio/extract-abbreviation'
+		'@emmetio/extract-abbreviation',
+		'@emmetio/css-snippets-resolver'
 	],
 	targets: [
 		{ format: 'cjs', dest: 'dist/emmet-codemirror-plugin.cjs.js' },

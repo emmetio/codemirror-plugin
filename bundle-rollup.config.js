687c68a76804a8b20371ad54ddca803bfc66ed30
Priyank Parashar <priyank@one.com>
2017-07-01 15:07:58 +0400

    Now bundling a non-minified browser version as well (using rollup@^0.43.0)

--- bundle-rollup.config.js
+++ bundle-rollup.config.js
@@ -4,15 +4,25 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import buble from 'rollup-plugin-buble';
 import uglify from 'rollup-plugin-uglify';
 
-export default {
+const configNonMinified = {
 	entry: './browser.js',
 	plugins: [
 		nodeResolve(),
-		buble(),
-		uglify()
+		buble()
 	],
 	moduleName: 'emmetCodeMirrorPlugin',
 	format: 'umd',
 	dest: 'dist/emmet-codemirror-plugin.js',
 	sourceMap: 'dist/emmet-codemirror-plugin.js.map'
 };
+
+const configMinified = Object.assign({}, configNonMinified);
+configMinified.plugins = configMinified.plugins.concat(uglify());
+configMinified.dest = 'dist/emmet-codemirror-plugin.min.js';
+configMinified.sourceMap = 'dist/emmet-codemirror-plugin.min.js.map';
+
+// Exporting multiple configs. Reference: https://github.com/rollup/rollupjs.org/blob/922d6abdffaa9584df88e5444529d917144c01f3/rollup.config.js
+export default [
+	configNonMinified,
+	configMinified
+];

c8eaa60c6edcdadd4241d80735e43c0913e449dc
Tony Onodi <tonyonodi@gmail.com>
2018-02-14 22:40:41 +0000

    Compiling with bublé and node-resolve.

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,26 @@
-'use strict';
+"use strict";
 
-import nodeResolve from 'rollup-plugin-node-resolve';
-import buble from 'rollup-plugin-buble';
-import uglify from 'rollup-plugin-uglify';
+import nodeResolve from "rollup-plugin-node-resolve";
+import buble from "rollup-plugin-buble";
+import uglify from "rollup-plugin-uglify";
 
-export default [{
-	input: './extension.js',
-	external: [
-		'@emmetio/expand-abbreviation',
-		'@emmetio/extract-abbreviation',
-		'@emmetio/css-snippets-resolver',
-		'@emmetio/html-matcher',
-		'@emmetio/stream-reader',
-		'@emmetio/stream-reader-utils',
-	],
-	output: [
-		{ format: 'cjs', file: 'dist/emmet-codemirror-plugin.cjs.js' },
-		{ format: 'es', file: 'dist/emmet-codemirror-plugin.es.js' },
-	]
-}, {
-	input: './browser.js',
-	plugins: [
-		nodeResolve(),
-		buble(),
-		uglify()
-	],
-	output: {
-		name: 'emmetCodeMirrorPlugin',
-		format: 'umd',
-		file: 'dist/emmet-codemirror-plugin.js',
-		sourcemap: 'dist/emmet-codemirror-plugin.js.map'
-	}
-}];
+export default [
+	{
+		input: "./extension.js",
+		plugins: [nodeResolve(), buble()],
+		output: [
+			{ format: "cjs", file: "dist/emmet-codemirror-plugin.cjs.js" },
+			{ format: "es", file: "dist/emmet-codemirror-plugin.es.js" },
+		],
+	},
+	{
+		input: "./browser.js",
+		plugins: [nodeResolve(), buble(), uglify()],
+		output: {
+			name: "emmetCodeMirrorPlugin",
+			format: "umd",
+			file: "dist/emmet-codemirror-plugin.js",
+			sourcemap: "dist/emmet-codemirror-plugin.js.map",
+		},
+	},
+];

0a4e4ff6bbef92e1de9cdafd98a9338f85904a08
Sergey Chikuyonok <serge.che@gmail.com>
2020-04-22 23:22:46 +0300

    Do not validate snippet name in aprsing mode

--- src/mode/snippet.ts
+++ src/mode/snippet.ts
@@ -1,37 +1,25 @@
-import { error, ParseModeError } from './utils';
-
-interface EmmetSnippetModeState {
-    parseError?: ParseModeError;
-}
-
 /**
  * Emmet snippet name parsing mode
  */
-export default function snippetNameMode(): CodeMirror.Mode<EmmetSnippetModeState> {
+export default function snippetNameMode(): CodeMirror.Mode<{}> {
     return {
-        startState() {
-            return {};
-        },
-
-        token(stream, state) {
-            if (!state.parseError) {
-                if (stream.eatWhile(ident)) {
-                    return 'tag';
-                }
-
-                if (stream.eat(separator)) {
-                    return 'operator';
-                }
+        token(stream) {
+            if (stream.eatWhile(ident)) {
+                return 'tag';
             }
 
-            state.parseError = error('Unexpected character', stream);
+            if (stream.eat(separator)) {
+                return 'operator';
+            }
+
+            stream.skipToEnd();
             return 'invalidchar';
         }
     };
 }
 
 function ident(ch: string): boolean {
-    return /[a-zA-Z0-9-_$@!%:]/.test(ch);
+    return /[a-zA-Z0-9-_$@!:]/.test(ch);
 }
 
 function separator(ch: string): boolean {
